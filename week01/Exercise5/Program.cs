0b40b286233f1a2b57bb5d0ca3473aefd9495d4e
larryg21 <lgomezvillamil@byupathway.edu>
2025-07-09 22:17:49 -0400

    I finished the Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
+
+    //Functions
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int usernumber = PromptUserNumber();
+
+        int Squarenumber = SquareNumber(usernumber);
+
+        DisplayResult(name, Squarenumber);
+
     }
+
+    static void DisplayWelcome()
+
+    {
+        Console.WriteLine("Welcome to the Program. ");
+    }
+
+    static string PromptUserName()
+
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+
+    {
+        Console.Write("Please enter your favourite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int usernumber)
+
+    { 
+        int square = usernumber * usernumber;
+
+        return square;
+    }
+
+    static void DisplayResult(String name, int square)
+
+    {
+    Console.WriteLine($"{name}, The square of your number is: {square}");
+    }
+    
 }
