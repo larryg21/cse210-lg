a6d122a524c069bf627458a10a8738108c3b2155
larryg21 <lgomezvillamil@byupathway.edu>
2025-07-13 21:31:11 -0400

    new folders

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,20 @@
+// Job.cs
+namespace Resumes
+{
+    public class Job
+    {
+        public string _company = "";
+        public string _jobTitle = "";
+        public int _startYear;
+        public string _endYear = "";
+
+        // Method to present the job's information.
+        public void DisplayJobDetails()
+        {
+
+
+        }
+    }
+}
+
+
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
+using Resumes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Inventory Control Coordinator";
+        job1._company = "DHL Logistics";
+        job1._startYear = 2022;
+        job1._endYear = "2024";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Inventory Control Supervisor";
+        job2._company = "Trillium2 Supply Chain";
+        job2._startYear = 2024;
+        job2._endYear = "Present";
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+        
     }
 }
